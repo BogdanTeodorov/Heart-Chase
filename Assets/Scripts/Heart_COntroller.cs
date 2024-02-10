c53b4c92eddc5bf1f22ca82a0302d09dae25c20d
Bogdan Teodorov <teodorovbogdan5@gmail.com>
2024-02-10 16:57:11 -0400

    Heart text is now rendered

--- Assets/Scripts/Heart_COntroller.cs
+++ Assets/Scripts/Heart_COntroller.cs
@@ -8,14 +8,14 @@ public class Heart_Controller : MonoBehaviour
 {
     int heartCounter;
     [SerializeField] AudioClip pickUpEffect;
-    // [SerializeField] TextMeshPro heartCountRender;
     public Text heartCounterText; // Reference to the UI Text component
+    [SerializeField] TextMeshProUGUI heartCountRender;
 
     // Start is called before the first frame update
     void Start()
     {
         heartCounter = 0;
-        // UpdateHeartCounterText(); // Update the text content when the game starts
+        UpdateHeartCounterText(); // Update the text content when the game starts
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -25,20 +25,18 @@ public class Heart_Controller : MonoBehaviour
             heartCounter++;
             GetComponent<AudioSource>().PlayOneShot(pickUpEffect);
             Debug.Log("Heart has been picked up " + heartCounter);
-            Destroy(other.gameObject);
-            // UpdateHeartCounterText(); // Update the text content when a heart is picked up
+            UpdateHeartCounterText(); // Update the text content when a heart is picked up
         }
+        Destroy(other.gameObject);
+
     }
 
     // Update the UI Text content with the current heart counter
-    // void UpdateHeartCounterText()
-    // {
-    //     if (heartCounterText != null && heartCountRender != null) // Ensure the reference is not null
-    //     {
-    //         heartCounterText.text = "Hearts: " + heartCounter.ToString();
-    //         heartCountRender.text = heartCounterText.text;
-    //     }
-    // }
+    void UpdateHeartCounterText()
+    {
+        heartCountRender.text = "Hearts: " + heartCounter.ToString();
+
+    }
 
     // Update is called once per frame
     void Update()
